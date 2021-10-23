fcf23b057c7270126d0da0ce32d5f1d72a5e2117
lucroselen <mriliev@outlook.com>
2021-10-24 00:17:32 +0300

    added validations

--- models/Course.js
+++ models/Course.js
@@ -3,16 +3,16 @@ const courseSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: true,
+      required: [true, "You must fill in the title!"],
       minlength: 6,
     },
     description: {
       type: String,
-      required: true,
+      required: [true, "You must fill in the description!"],
     },
     imageUrl: {
       type: String,
-      required: true,
+      required: [true, "You must fill in the Image Url!"],
       validate: [/^https?:\/\//i, "URL must start with http:// or https://"],
       minlength: 10,
     },
